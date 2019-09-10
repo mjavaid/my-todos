d0708920c96de70fa78ef241b77bb0420223fdb9
Sajawal Javaid <sajawaljavaid@hotmail.com>
2019-09-09 23:05:36 -0400

    Add basic storage
    * Use browser localstorage to persist list items

--- src/app/services/todo-list.service.ts
+++ src/app/services/todo-list.service.ts
@@ -1,7 +1,7 @@
+import { TodoList } from './../models/todo-list.model';
 import { TodoListItem } from './../models/todo-list-item.model';
 import { Injectable } from '@angular/core';
-import { ReplaySubject } from 'rxjs';
-import { TodoList } from '../models/todo-list.model';
+import { ReplaySubject, empty } from 'rxjs';
 
 @Injectable()
 export class TodoListService {
@@ -11,16 +11,35 @@ export class TodoListService {
   listUpdated: ReplaySubject<TodoList> = new ReplaySubject<TodoList>(1);
 
   constructor() {
-    this.todoList = new TodoList();
+    this.todoList = this.loadList();
   }
 
   addListItem(item: TodoListItem) {
     this.todoList.list.push(item);
     this.listUpdated.next(this.todoList);
+    this.saveList();
   }
 
   get list() {
     return this.todoList.list;
+  }
+
+  private saveList() {
+    window.localStorage.setItem('todo-list', JSON.stringify(this.todoList));
+  }
+
+  private loadList() {
+    const emptyList = new TodoList();
+    if (window.localStorage) {
+      try {
+        return JSON.parse(window.localStorage.getItem('todo-list')) || emptyList;
+      } catch (e) {
+        console.error('Could not load from localStorage:', e);
+        return emptyList;
+      }
+    } else {
+      return emptyList;
+    }
   }
 
 }
